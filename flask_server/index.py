0a365722a6de486bcdcf801e0a438bffe32004cc
Aakash Sheth <avsheth@mac-air-19.cs.iastate.edu>
2016-05-05 20:01:38 -0500

    One time

--- flask_server/index.py
+++ flask_server/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, jsonify
 import RPi.GPIO as GPIO
 import time
 import myDHT22
@@ -20,174 +20,173 @@ GPIO.setup(18, GPIO.OUT)
 
 app = Flask(__name__)
 
+
 @app.route("/test", methods=['GET'])
 def test():
-	data = "{'status': 'It works'}"
-       	return data
+    data = "{'status': 'It works'}"
+    return data
+
 
 #######################################################
+# Render the homepage
+
+@app.route("/homepage", methods=['GET'])
+def homepage(name=None):
+    return render_template('index.html', name=name)
+
 
 # Get the current humidity
-@app.route("/humidity_sensor", methods=['GET','POST'])
+@app.route("/humidity_sensor", methods=['GET', 'POST'])
 def getHumiditySensor():
-        
-	
-	# Get the humidity sensor reading
-        reading = myDHT22.getHumidity(1)
-	print(reading['humidity'])
-	time = reading['datetime']
-        current_humidity = reading['humidity']
-        current_temp = reading['temp']
-	print(reading)
+    # Get the humidity sensor reading
+    reading = myDHT22.getHumidity(1)
+    print(reading['humidity'])
+    time = reading['datetime']
+    current_humidity = reading['humidity']
+    current_temp = reading['temp']
+    print(reading)
 
-        data = {
-                'status' : '',
-                'time' : '',
-                'current_humidity' : '',
-                'current_temp' : '',
-        }
+    data = {
+        'status': '',
+        'time': '',
+        'current_humidity': '',
+        'current_temp': '',
+    }
 
-        if(reading):
-                data['status'] = 'success'
-                data['time'] = time
-                data['current_humidity'] = current_humidity
-                data['current_temp'] = current_temp
-		print("Data:", str(data)) 
-                return str(data)
-
-
-        data['current_humidity'] = "null"
-        data['current_temp'] = "null"
-        data['status'] = "fail"
+    if (reading):
+        data['status'] = 'success'
         data['time'] = time
-        data['error'] = "Unable to read from sensor"
-        
+        data['current_humidity'] = current_humidity
+        data['current_temp'] = current_temp
+        print("Data:", str(data))
         return str(data)
 
-        
+    data['current_humidity'] = "null"
+    data['current_temp'] = "null"
+    data['status'] = "fail"
+    data['time'] = time
+    data['error'] = "Unable to read from sensor"
+
+    return str(data)
+
 
 @app.route("/change_humidity_setting/<setting>", methods=['GET', 'POST'])
 def setCurrentHumidity(setting):
+    print ("Change humidity settings....")
+    # Set new value
+    value = setting
+    print("value", value)
 
-        print ("Change humidity settings....")
-        # Set new value
-	value = setting
-	print("value", value)
+    current_humidity_setting = ''
 
-        current_humidity_setting = ''
-        
-        try:
-                files.writeSettings(value)
-                AutoHumidity.perform_check(-1)
-                current_humidity_setting = value
-
-                data = { 
-			'status' : 'success',
-			'current_humidity_setting' : str(current_humidity_setting)
-		}
-                return str(data)
-                
-        except Exception:
-                data = {
-                        'status' : 'failure',
-                        'message' : 'Could not write to file'
-                }
-        return str(data)
-                
-
-# Get the current humidity setting
-@app.route("/humidity_setting", methods=['GET','POST'])
-def getCurrentHumiditySetting():
-
-        print ("Get Humidity Setting....")
-        # Get the current humidity setting
-        try:
-                value = files.readSettings()
-
-                current_humidity_setting = value
-
-                data = {
-                        'status' : 'success',
-                        'current_humidity_setting' : current_humidity_setting,
-                }
-                return str(data)
-                
-        except Exception:
-                return "{'status' : 'failure','message' : 'Could not read from file'})"
-
-# User updated on/off
-@app.route("/user_state/<state>", methods=['GET','POST'])
-def setUserState(state):
-
-        # Set new value
-	value = state
-
-        try:
-                files.writeUserState(value)
-		AutoHumidity.perform_check(-2)
-                current_user_state = value
-
-                data = { 
-			'status' : 'success',
-			'current_user_state' : str(current_user_state)
-		}
-                return str(data)
-                
-        except Exception:
-                return "{'status' : 'failure','message' : 'Could not write user_state from file'})"
-
-
- 
-# Get current state of humidifier
-@app.route("/user_state", methods=['GET','POST'])
-def getCurrentState():
-
+    try:
+        files.writeSettings(value)
+        AutoHumidity.perform_check(-1)
+        current_humidity_setting = value
 
         data = {
-                'status' : '',
-                'current_user_state' : '',
+            'status': 'success',
+            'current_humidity_setting': str(current_humidity_setting)
         }
-        
-        # Get the status, on or off
-        try:
-                state = files.readUserState()
+        return str(data)
 
-                data['status'] = 'success'
-                data['current_user_state'] = state
-                return str(data)
-                
-        except Exception:
-                return "{'status' : 'failure','message' : 'Could not read humidifier state from file'})"
+    except Exception:
+        data = {
+            'status': 'failure',
+            'message': 'Could not write to file'
+        }
+    return str(data)
+
+
+# Get the current humidity setting
+@app.route("/humidity_setting", methods=['GET', 'POST'])
+def getCurrentHumiditySetting():
+    print ("Get Humidity Setting....")
+    # Get the current humidity setting
+    try:
+        value = files.readSettings()
+
+        current_humidity_setting = value
+
+        data = {
+            'status': 'success',
+            'current_humidity_setting': current_humidity_setting,
+        }
+        return str(data)
+
+    except Exception:
+        return "{'status' : 'failure','message' : 'Could not read from file'})"
+
+
+# User updated on/off
+@app.route("/user_state/<state>", methods=['GET', 'POST'])
+def setUserState(state):
+    # Set new value
+    value = state
+
+    try:
+        files.writeUserState(value)
+        AutoHumidity.perform_check(-2)
+        current_user_state = value
+
+        data = {
+            'status': 'success',
+            'current_user_state': str(current_user_state)
+        }
+        return str(data)
+
+    except Exception:
+        return "{'status' : 'failure','message' : 'Could not write user_state from file'})"
+
+
+# Get current state of humidifier
+@app.route("/user_state", methods=['GET', 'POST'])
+def getCurrentState():
+    data = {
+        'status': '',
+        'current_user_state': '',
+    }
+
+    # Get the status, on or off
+    try:
+        state = files.readUserState()
+
+        data['status'] = 'success'
+        data['current_user_state'] = state
+        return str(data)
+
+    except Exception:
+        return "{'status' : 'failure','message' : 'Could not read humidifier state from file'})"
+
 
 # Display all current settings
-@app.route("/all_settings", methods=['GET','POST'])
+@app.route("/all_settings", methods=['GET', 'POST'])
 def getAllSettings():
+    user_state = files.readUserState()
+    to_show = "User State:\t" + user_state
 
-        user_state = files.readUserState()
-        to_show =       "User State:\t"+user_state
+    state = files.readState()
+    to_show += "<br>Humidifier State:\t" + state
 
-        state = files.readState()
-        to_show +=      "<br>Humidifier State:\t"+state
+    value = files.readSettings()
+    to_show += "<br>Humidity Setting:\t" + value
 
-        value = files.readSettings()
-        to_show +=      "<br>Humidity Setting:\t"+value
+    reading = myDHT22.getHumidity(1)
+    hum = reading['humidity']
+    to_show += "<br>Current Humidity:\t" + str(hum)
 
-        reading = myDHT22.getHumidity(1)
-	hum = reading['humidity']
-	to_show +=	"<br>Current Humidity:\t"+str(hum)
+    data = {
+        'status': 'success',
+        'current_state': state,
+        'current_user_state': user_state,
+        'current_humidity_setting': value,
+        'current_humidity': hum,
+    }
 
-	data = {
-                'status' : 'success',
-                'current_state' : state,
-                'current_user_state' : user_state,
-                'current_humidity_setting' : value,
-                'current_humidity' : hum,
-        }
-
-	return str(data)
+    return str(data)
 
 
 if __name__ == "__main__":
-	#app.run(processes=3)
-        app.run(host='0.0.0.0', processes=3)
-        print("Succesfully launched!")
+    # app.run(processes=3)
+    app.run(host='0.0.0.0', processes=3)
+    print("Succesfully launched!")
---
+++ flask_server/static/js/script.js
@@ -0,0 +1,140 @@
+$(function(){
+
+	var onlineStatus = $("#header_status");
+	var onButton = $("#turn_on");
+	var offButton = $("#turn_off");
+	var setButton = $('#set_button');
+	var humidityAmount = $('#humidity_amount');
+
+	onButton.hide()
+	offButton.hide()
+	
+	// Get the data the firs time the server runs
+	
+		$.ajax({
+			url: "/data",
+			success: function(result){
+				console.log(result);
+				
+				// Add two random numbers for each dataset
+				myLiveChart.addData([result['current_humidity'],result['current_temp']], ++latestLabel);
+				// Remove the first point so we dont just add values forever
+				myLiveChart.removeData();
+
+				// Update the UI
+				$('#current_setting').html(result['current_setting']);
+				$('#localIP').html(result['current_ip']);
+				if(result['current_status'] === 'ON')
+				{
+					onlineStatus.css('color', 'green');
+					onButton.hide()
+					offButton.show()
+				}
+				else {
+					onlineStatus.css('color', 'red');
+					onButton.show()
+					offButton.hide()
+				}
+
+				//$('#localIP').html(result['ip']);
+			}
+		})
+	var canvas = document.getElementById('myChart'),
+    ctx = canvas.getContext('2d'),
+    startingData = {
+      labels: [1, 2, 3, 4, 5, 6, 7],
+      datasets: [
+      		{
+				fillColor: "rgba(220,220,220,0.2)",
+				strokeColor: "rgba(220,220,220,1)",
+				pointColor: "rgba(220,220,220,1)",
+				pointStrokeColor: "#fff",
+				data: [65, 59, 80, 81, 56, 55, 40]
+			},
+        	{
+              fillColor: "rgba(220,220,220,0.2)",
+              strokeColor: "rgba(220,220,220,1)",
+              pointColor: "rgba(220,220,220,1)",
+              pointStrokeColor: "#fff",
+              data: [65, 59, 80, 81, 56, 55, 40]
+          	}
+        ]
+    },
+    latestLabel = startingData.labels[6];
+
+	// Reduce the animation steps for demo clarity.
+	var myLiveChart = new Chart(ctx).Line(startingData, {animationSteps: 15});
+
+
+	setInterval(function(){
+		getData();	  
+	}, 1500);
+
+	function getData(){
+		$.ajax({
+			url: "/data",
+			success: function(result){
+				console.log(result);
+				
+				// Add two random numbers for each dataset
+				myLiveChart.addData([result['current_humidity'],result['current_temp']], ++latestLabel);
+				// Remove the first point so we dont just add values forever
+				myLiveChart.removeData();
+
+				// Update the UI
+				$('#current_setting').html(result['current_setting']);
+				
+				if(result['current_status'] === 'ON')
+				{
+					onlineStatus.css('color', 'green');
+					onButton.hide()
+					offButton.show()
+				}
+				else {
+					onlineStatus.css('color', 'red');
+					onButton.show()
+					offButton.hide()
+				}
+
+				//$('#localIP').html(result['ip']);
+			}
+		})
+	}
+
+	onButton.on('click', function(){
+		// Send AJAX ON request
+		alert("ON Button clicked");
+
+		$.ajax({
+			url: "/user_state/on",
+			success: function(result){
+				console.log(result);
+			}
+		})
+	});
+
+	offButton.on('click', function(){
+		// Send AJAX OFF request
+		alert("OFF Button clicked");
+
+		$.ajax({
+			url: "/user_state/off",
+			success: function(result){
+				console.log(result);
+			}
+		})
+	});
+
+	setButton.on('click', function(){
+		// Set the humidity to the desired setting
+		var url = "/change_humidity_setting/" + humidity_amount.val()
+
+		$.ajax({
+			url: url,
+			success: function(result){
+				console.log(result);
+			}
+		})
+	});
+})
+
